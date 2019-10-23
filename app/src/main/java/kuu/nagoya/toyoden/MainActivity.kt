7c2bfd62b9993a1926a1a7dd07c7a6570d7df925
fumiya-kume <fumiya.kume@hotmail.com>
2019-10-24 00:07:05 +0900

    add app

---
+++ app/src/main/java/kuu/nagoya/toyoden/MainActivity.kt
@@ -0,0 +1,19 @@
+package kuu.nagoya.toyoden
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kuu.nagoya.toyoden.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+
+}
---
+++ app/src/main/java/kuu/nagoya/toyoden/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package kuu.nagoya.toyoden.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
